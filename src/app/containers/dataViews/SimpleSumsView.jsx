1c6df5a9fd9e8661d9012686da0aec6da593b605
Boris Searles <boris@lucidgardens.com>
2016-08-02 18:26:57 +0200

    add reselect lib and use for calculating transaction sums

--- src/app/containers/dataViews/SimpleSumsView.jsx
+++ src/app/containers/dataViews/SimpleSumsView.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 
-const SimpleSumsView = ({ transactions }) => (
+const transactionSelector = state => state.transactions;
+const totalExpenseSelector = createSelector(
+  transactionSelector,
+  (items) => items.reduce((acc, item) => acc + (item.amount || 0), 0)
+);
+
+const SimpleSumsView = ({ transactionCount, expenseSum, incomeSum }) => (
   <div id="view-simple-sums">
     <span className="income">Total Income: 0,00 EUR</span>
-    <span className="expenses">Total Expenses: 0,00 EUR</span>
-    <span className="count">Transactions: {transactions.length}</span>
+    <span className="expenses">Total Expenses: {expenseSum},00 EUR</span>
+    <span className="count">Transactions: {transactionCount}</span>
   </div>
 );
 
 const mapStateToProps = (state) => {
   return {
-    transactions: state.transactions
+    transactionCount: state.transactions.length,
+    expenseSum: totalExpenseSelector(state),
+    incomeSum: 0
   };
 };
 
