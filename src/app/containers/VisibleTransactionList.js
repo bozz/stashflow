ba9b16fb9057b4dacd610a3f7e6fcc3110810ea9
Boris Searles <boris@lucidgardens.com>
2016-08-03 09:25:36 +0200

    use reselect to improve visible transactions sorting

--- src/app/containers/VisibleTransactionList.js
+++ src/app/containers/VisibleTransactionList.js
@@ -1,22 +1,29 @@
 import { connect } from 'react-redux';
+import { createSelector } from 'reselect';
 import * as actions from '../actions';
 import TransactionList from '../components/TransactionList';
 
-// TODO: find better way to handle sorting - use reselect!
-const sortTransactions = transactions => {
-  return transactions.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    } else if (a.date > b.date) {
-      return -1;
-    }
-    return 0;
-  });
-};
+const transactionsSelector = state => state.transactions;
+const sortKeySelector = state => 'date';
+
+const sortTransactionsSelector = createSelector(
+  transactionsSelector,
+  sortKeySelector,
+  (transactions, sortKey) => {
+    return transactions.sort((a, b) => {
+      if (a[sortKey] < b[sortKey]) {
+        return 1;
+      } else if (a[sortKey] > b[sortKey]) {
+        return -1;
+      }
+      return 0;
+    });
+  }
+);
 
 const mapStateToProps = (state) => {
   return {
-    transactions: sortTransactions(state.transactions)
+    transactions: sortTransactionsSelector(state)
   };
 };
 const mapDispatchToProps = (dispatch) => {
