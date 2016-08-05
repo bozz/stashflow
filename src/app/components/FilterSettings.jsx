3e971e8a6a68d79042a2ac900702740371f3d2dc
Boris Searles <boris@lucidgardens.com>
2016-08-05 18:01:22 +0200

    some style tweaking to filter-settings

--- src/app/components/FilterSettings.jsx
+++ src/app/components/FilterSettings.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 import moment from 'moment';
 import Section from './Section';
 
-export default (props) => {
+export default ({ filter }) => {
   return (
-    <Section id="fitler-settings">
-      <p>Settings... {props.name}</p>
+    <Section id="filter-settings">
+      <p>Settings... {filter.name}</p>
     </Section>
   );
 };
