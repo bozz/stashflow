c661d24b7149f65ee9da249b3e8ba46fd81f0e7a
Boris Searles <boris@lucidgardens.com>
2019-05-30 23:35:45 +0200

    fix hot-loading

--- client/src/app.js
+++ client/src/app.js
@@ -1,59 +1,46 @@
-/* global document */
-
-import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { AppContainer } from 'react-hot-loader';
+import { BrowserRouter } from 'react-router-dom';
 
-import configureStore from './config/store';
-
-import MainLayout from './views/layout/Main';
-
-let store = null;
-let isInitialized = false;
-
-axios.all([
-    axios.get('http://localhost:4000/accounts'),
-    axios.get('http://localhost:4000/categories')
-  ])
-  .then(axios.spread((accountsRes, categoriesRes) => {
-    store = configureStore({
-      accounts: accountsRes.data,
-      categories: categoriesRes.data
-    });
-    isInitialized = true;
-    render(MainLayout);
-  }))
-  .catch((error) => {
-    console.log("Error: ", error);
-  });
+// let store = null;
+// let isInitialized = false;
+// axios.all([
+//     axios.get('http://localhost:4000/accounts'),
+//     axios.get('http://localhost:4000/categories')
+//   ])
+//   .then(axios.spread((accountsRes, categoriesRes) => {
+//     store = configureStore({
+//       accounts: accountsRes.data,
+//       categories: categoriesRes.data
+//     });
+//     isInitialized = true;
+//     render(MainLayout);
+//   }))
+//   .catch((error) => {
+//     console.log("Error: ", error);
+//   });
 
 
-const render = (Component) => {
+function renderApp() {
+  const configureStore = require("./config/store").default;
+  const MainLayout = require("./views/layout/Main").default;
+
   ReactDOM.render(
-    <AppContainer>
-      {isInitialized ? (
-      <Provider store={store}>
-        <BrowserRouter>
-          <Component />
-        </BrowserRouter>
-      </Provider>
-      ) : (
-        <h1>Initializing...</h1>
-      )}
-    </AppContainer>
+    <Provider store={configureStore()}>
+      <BrowserRouter>
+        <MainLayout />
+      </BrowserRouter>
+    </Provider>
     , document.getElementById('root')
   );
-};
+}
 
-render(MainLayout);
+renderApp();
 
 if (module.hot) {
-  module.hot.accept('./views/layout/Main', () => {
-    const NewClient = require('./views/layout/Main').default; // eslint-disable-line global-require
-    render(NewClient);
+  module.hot.accept(() => {
+    renderApp();
   });
 }
 
